44eb31309b71bce94686bde73be84839613faf72
Joe Sak <joe@joesak.com>
2015-01-13 11:15:14 -0500

    Add a Codemaster who creates the code on start

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -2,16 +2,24 @@ module Mastermind
   class Player
   end
 
+  class Codemaster
+    def create_code
+      'RGBY'
+    end
+  end
+
   class Game
-    attr_reader :player
+    attr_reader :player, :codemaster, :code
 
     def initialize
       @started = false
       @player = Player.new
+      @codemaster = Codemaster.new
     end
 
     def start
       @started = true
+      @code = codemaster.create_code
     end
 
     def started?
--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -4,13 +4,22 @@ RSpec.describe Mastermind::Game do
   it 'can be started' do
     game = described_class.new
     game.start
-
     expect(game).to be_started
   end
 
   it 'has a player' do
     game = described_class.new
-
     expect(game.player).not_to be_nil
+  end
+
+  it 'has a codemaster' do
+    game = described_class.new
+    expect(game.codemaster).not_to be_nil
+  end
+
+  it 'starts the game by the codemaster creating a code' do
+    game = described_class.new
+    game.start
+    expect(game.code).not_to be_nil
   end
 end
